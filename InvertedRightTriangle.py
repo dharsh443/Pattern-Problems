3129b51b473b481a771dcfa36c7617637692e7fd
DharshVsgn <110477705+dharsh443@users.noreply.github.com>
2024-02-23 23:39:51 +0530

    Add files via upload

---
+++ InvertedRightTriangle.py
@@ -0,0 +1,8 @@
+n=int(input("Enter the no of rows:"))
+for i in range (n,0,-1):
+    for j in range(0,n-i):
+        print(end="")
+    for k in range(0,i):
+        print("*",end=" ")
+    print()
+        
