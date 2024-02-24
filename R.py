ab06b8b4a12533cff25fd35e9743ff96be444ebd
DharshVsgn <110477705+dharsh443@users.noreply.github.com>
2024-02-25 01:20:01 +0530

    Add files via upload

---
+++ R.py
@@ -0,0 +1,8 @@
+for row in range(6):
+    for col in range(5):
+        if(col==0 or col==4 and (row!=3 and row!=4 and row!=5) or row==0 or row==2 and
+           (col>0 or col<5)or row==3 and (col!=2 and col!=3 and col!=4) or row==4 and (col!=0 and col!=1 and col!=3 and col!=4) or row==5 and (col!=0 and col!=1 and col!=2 and col!=4)):
+            print("*", end="")
+        else:
+            print(end=" ")
+    print()
